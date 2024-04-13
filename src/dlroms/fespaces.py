330d824953158a63b8443cae32b69b059cf146bd
Luigi Pagani <127208537+LuigiPagani@users.noreply.github.com>
2024-04-13 15:46:50 +0200

    Update fespaces.py

--- src/dlroms/fespaces.py
+++ src/dlroms/fespaces.py
@@ -45,12 +45,9 @@ try:
     inner = dolfin.inner
     div = dolfin.div
 except:
-    print("Installing dolfin...")
     import os
-    print("Installling Fenics")
     os.system('wget "https://fem-on-kaggle.github.io/releases/fenics-install-real.sh" -O "/tmp/fenics-install.sh"')
     os.system('bash "/tmp/fenics-install.sh"')
-    print("Installing gmsh")
     os.system('wget "https://fem-on-colab.github.io/releases/gmsh-install.sh" -O "/tmp/gmsh-install.sh" && bash "/tmp/gmsh-install.sh"')
 
 
